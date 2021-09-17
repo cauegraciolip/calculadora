8cf87d90b09659dc1b819ec3286ec80966641466
cauegraciolip <cauegracioli@outlook.com>
2021-09-16 22:13:51 -0300

    inicio javascript

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,32 @@
+'use strict'
+
 const display = document.querySelector('#contentDisplay')
-const botao = document.querySelectorAll("div[class=btn]")
+const numeros = document.querySelectorAll('[id*=tecla]')
+const reset = document.getElementById('reset')
+const apagar = document.getElementById('delete')
 
+// pegar o botao e colocar no display
 
+const inserirNumero = (evento) => {
+    display.textContent += evento.target.textContent
+}
 
-console.log(display)
-console.log(botao)
+numeros.forEach(numero => 
+    numero.addEventListener('click', inserirNumero)
+)
+
+// função reset = limpar numeros do display
+
+const limparDisplay = () => display.textContent = ""
+
+reset.addEventListener('click', limparDisplay)
+
+// função delete = apagar ultimo numero inserido no display
+
+const apagarNumero = () => {
+    let str = document.getElementById('contentDisplay').textContent
+
+    str = str.slice(0, -1)
+}
+
+apagar.addEventListener('click', apagarNumero)
